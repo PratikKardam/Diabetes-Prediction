02f0d70738c56eee377c36554fa5350e2e2d9bd4
Pratik <140505644+PratikKardam@users.noreply.github.com>
2026-02-04 00:34:26 +0530

    initial commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
       age
     };
 
-    const res = await fetch("http://127.0.0.1:5000/predict", {
+    const res = await fetch("https://diabetes-prediction-jgrx.onrender.com/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
